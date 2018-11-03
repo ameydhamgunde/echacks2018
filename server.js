1972d460a4d5cd246ae1874b0b249beab03db7ce
Amey Dhamgunde <ameydhamgunde@yahoo.com>
2018-11-03 01:16:54 -0400

    basically done all the file structure and stuff, now others can work on their respective parts

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static('css')) // this is needed so for the css file to be recognized
 app.set('view engine', 'ejs')
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.render('index'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
